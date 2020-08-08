4a56b658d56d20e2f3d236dc6a4f05c6caa5fd32
Mateus-Franceschina <mateusfranceschina@gmail.com>
2020-08-08 11:55:20 -0300

    mobile interface

---
+++ mobile/src/pages/Teach/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, ImageBackground, Text } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+
+import teachClassesBgImage from '../../assets/images/give-classes-background.png'
+
+import styles from './styles'
+import { useNavigation } from '@react-navigation/native'
+
+function Teach() {
+  const {goBack} = useNavigation()
+
+  function handeNavigateBack() {
+    goBack()
+  }
+
+
+  return (
+    <View style={styles.container}> 
+      <ImageBackground resizeMode='contain' source={teachClassesBgImage} style={styles.content} >
+        <Text style={styles.title}>Quer ser um Proffy</Text>
+        <Text style={styles.description}>Para começar, você precisa se cadastrar como professor na plataforma web</Text>
+      </ImageBackground>
+
+      <RectButton onPress={handeNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo Bem</Text>
+      </RectButton>
+    </View>
+  )
+}
+
+export default Teach
